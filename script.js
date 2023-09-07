eda7860c64d7423a268a81892357a64b0c76b56a
Elhanan Avihail <etsAvihail@gmail.com>
2023-09-07 07:48:43 +0300

    re-order and fix const names

--- script.js
+++ script.js
@@ -1,38 +1,23 @@
 'use strict'
-/*
-A. Enter to the Zoom meeting from the Zoom Web Client - not the desktop app!
-(
- To open Zoom in the Web client:
-    Click on the Zoom link.
-    Close the confirm popup.
-    Click on the "Launch Meeting" button.
-    Close the confirm popup again.
-    Now you can click on the "Join from Your Browser" link in the bottom of the site window.
-)
-B. copy all this script.
-C. Open the consols, paste and run this script.
-D. Now copy the full string from the bottom of the console.
-E. Vualla!!
-*/
-
-const participantsContainerSelector = '.participants-section-container'
-const participantSelector = '[id^="participants-list-"] .participants-item__display-name'
+const AUTOMATICALLY_REMOVE_NAMES = [
+    'Coding Academy',
+    'Coding-Academy'
+]
+const PARTICIPANTS_CONTAINER_SELECTOR = '.participants-section-container'
+const PARTICIPANT_SELECTOR = '[id^="participants-list-"] .participants-item__display-name'
 
 try {
-    const elParticipantsContainer = document.querySelector(participantsContainerSelector)
+    const elParticipantsContainer = document.querySelector(PARTICIPANTS_CONTAINER_SELECTOR)
     if (!elParticipantsContainer) throw new Error()
 
     elParticipantsContainer.style.height = '50000px' // set place for zoom to render all participants
 
-    const elParticipants = [...document.querySelectorAll(participantSelector)]
+    const elParticipants = [...document.querySelectorAll(PARTICIPANT_SELECTOR)]
     const allParticipantNames = elParticipants.map((p) => p.innerText) // get all names
 
-    const toRemoveNames = [
-        'Coding Academy',
-        'Coding-Academy'
-    ]
+
     const participantNames = allParticipantNames.filter(pName => {
-        return toRemoveNames.every(removeName => !pName.includes(removeName))
+        return AUTOMATICALLY_REMOVE_NAMES.every(removeName => !pName.includes(removeName))
     })
 
     const participantNamesStr = JSON.stringify(participantNames)
