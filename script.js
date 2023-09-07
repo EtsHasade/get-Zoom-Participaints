6d7fc3ba3870b42d2182ad04d0f282ecb0490fbe
Elhanan Avihail <etsAvihail@gmail.com>
2023-09-07 12:21:45 +0300

    cheng const to let - to enable reuse in the console

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict'
-const AUTOMATICALLY_REMOVE_NAMES = [
+let AUTOMATICALLY_REMOVE_NAMES = [
     /* 
     insert here the names that you want to automatically remove from the result list.
     for example, if your school called "Coding academy", and all your staff members called something like "XXX - Coding Academy" -  insert this names part: 
@@ -9,24 +9,24 @@ const AUTOMATICALLY_REMOVE_NAMES = [
     */
 ]
 
-const PARTICIPANTS_CONTAINER_SELECTOR = '.participants-section-container'
-const PARTICIPANT_SELECTOR = '[id^="participants-list-"] .participants-item__display-name'
+let PARTICIPANTS_CONTAINER_SELECTOR = '.participants-section-container'
+let PARTICIPANT_SELECTOR = '[id^="participants-list-"] .participants-item__display-name'
 
 try {
-    const elParticipantsContainer = document.querySelector(PARTICIPANTS_CONTAINER_SELECTOR)
+    let elParticipantsContainer = document.querySelector(PARTICIPANTS_CONTAINER_SELECTOR)
     if (!elParticipantsContainer) throw new Error()
 
     elParticipantsContainer.style.height = '50000px' // set place for zoom to render all participants
 
-    const elParticipants = [...document.querySelectorAll(PARTICIPANT_SELECTOR)]
-    const allParticipantNames = elParticipants.map((p) => p.innerText) // get all names
+    let elParticipants = [...document.querySelectorAll(PARTICIPANT_SELECTOR)]
+    let allParticipantNames = elParticipants.map((p) => p.innerText) // get all names
 
 
-    const participantNames = allParticipantNames.filter(pName => {
+    let participantNames = allParticipantNames.filter(pName => {
         return AUTOMATICALLY_REMOVE_NAMES.every(removeName => !pName.includes(removeName))
     })
 
-    const participantNamesStr = JSON.stringify(participantNames)
+    let participantNamesStr = JSON.stringify(participantNames)
         .replaceAll(',', '\n')
         .replaceAll('"', '')
         .replaceAll('[', '')
