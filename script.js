d6e61705021c39d0fd8221eee9fa9167fdff22b6
Elhanan Avihail <etsAvihail@gmail.com>
2023-09-07 07:08:09 +0300

    add exp how to open zoom web client

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+'use strict'
 /*
 A. Enter to the Zoom meeting from the Zoom Web Client - not the desktop app!
+(
+ To open Zoom in the Web client:
+    Click on the Zoom link.
+    Close the confirm popup.
+    Click on the "Launch Meeting" button.
+    Close the confirm popup again.
+    Now you can click on the "Join from Your Browser" link in the bottom of the site window.
+)
 B. copy all this script.
 C. Open the consols, paste and run this script.
 D. Now copy the full string from the bottom of the console.
