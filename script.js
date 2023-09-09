d14338cb22925c4a5d218bf5049dad61284bc959
Elhanan Avihail <etsAvihail@gmail.com>
2023-09-09 23:10:44 +0300

    more hight for the list, and some comments

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ try {
     let elParticipantsContainer = document.querySelector(PARTICIPANTS_CONTAINER_SELECTOR)
     if (!elParticipantsContainer) throw new Error()
 
-    elParticipantsContainer.style.height = '50000px' // set place for zoom to render all participants
+    // Zoom render the participants according to the list hight. set place for zoom to render all participants:
+    elParticipantsContainer.style.height = '500000px' 
 
     let elParticipants = [...document.querySelectorAll(PARTICIPANT_SELECTOR)]
     let allParticipantNames = elParticipants.map((p) => p.innerText) // get all names
@@ -33,7 +34,8 @@ try {
         .replaceAll(']', '')
         .replaceAll('\n\n', '\n')
 
-    console.log(participantNamesStr)
+    //print the list! 
+    console.log(participantNamesStr) // As a text
     participantNamesStr // In the console, this line will print the list as some element. and we can copy it with "Copy string content".
 
 } catch (err) {
